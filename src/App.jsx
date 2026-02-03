5903675f81eaf5eb0fb627ec0d78ba80a6fad39b
Tushar-Jadaun <tusharjadaun2001485@gmail.com>
2026-02-03 20:20:21 +0530

    add icons and create frontend

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { SiSolana, SiEthereum } from "react-icons/si";
+
+function App() {
+  return (
+    <div className="bg-gray-700 min-h-screen">
+      
+      {/* Header */}
+      <div className="flex items-center p-8 w-full font-bold text-white bg-gray-600 text-5xl">
+        Solana Wallet
+      </div>
+
+      {/* Main Section */}
+      <div className="w-full bg-gray-600 p-8 h-120">
+        <h1 className="text-4xl text-white">
+          Kosh supports multiple blockchains
+        </h1>
+
+        <h2 className="text-2xl text-white">
+          Choose a blockchain to get started.
+        </h2>
+
+        {/* Buttons */}
+        <div className="flex m-2 gap-3">
+
+          <button className="flex items-center gap-2 bg-white text-black p-3 rounded-2xl">
+            <SiSolana size={22} />
+            Solana
+          </button>
+
+          <button className="flex items-center gap-2 bg-white text-black p-3 rounded-2xl">
+            <SiEthereum size={22} />
+            Ethereum
+          </button>
+
+        </div>
+      </div>
+
+      {/* Footer */}
+      <div className="flex text-left text-white p-8">
+        Designed and Developed by Tushar Jadaun
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
